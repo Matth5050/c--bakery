13405ac520f18b2803f9c2e12bf3ef23a938e1f0
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-15 11:42:08 -0700

    added test for setting order amount

--- Menu.Tests/ModelTests/PastryTests.cs
+++ Menu.Tests/ModelTests/PastryTests.cs
@@ -15,25 +15,16 @@ namespace Pastry.Tests
       Assert.AreEqual(typeof(PastryOrders), newOrder.GetType());
     }
 
-    // [TestMethod]
-    // public void GetOrderAmount_ReturnsNumberOfLoaves_int()
-    // {
-    //   int testOrder = 3;
-    //   BreadOrders newBill = new BreadOrders(testOrder);
-    //   int result = newBill.OrderAmount;
-    //   Assert.AreEqual(testOrder, result);
-    // }
-
-    // [TestMethod]
-    // public void SetDescription_SetDescription_int()
-    // {
-    //   int description = 5;
-    //   BreadOrders newOrder = new BreadOrders(description);
-    //   int updateDescription = 20;
-    //   newOrder.OrderAmount = updateDescription;
-    //   int result = newOrder.OrderAmount;
-    //   Assert.AreEqual(updateDescription, result);
-    // }
+    [TestMethod]
+    public void SetOrderAmount_SetOrderAmount_int()
+    {
+      int orderedAmount = 5;
+      PastryOrders newOrder = new PastryOrders(orderedAmount);
+      int updateOrdredAmount = 20;
+      newOrder.OrderAmount = updateOrdredAmount;
+      int result = newOrder.OrderAmount;
+      Assert.AreEqual(updateOrdredAmount, result);
+    }
 
     // [TestMethod]
     // public void SumPrice_SumsTotalPrice_int()
