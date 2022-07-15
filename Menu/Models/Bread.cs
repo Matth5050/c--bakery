ffc58d759d93a5d0c4a53cf98bcca3f173c537b0
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-15 11:02:20 -0700

    added CheckDiscount() method

--- Menu/Models/Bread.cs
+++ Menu/Models/Bread.cs
@@ -6,17 +6,34 @@ namespace Bread.Models
   public class BreadOrders
   {
     public int OrderAmount { get; set; }
-    // public int Bill { get; set; }
+    public int Bill { get; set; }
+    public int FinalPrice { get; set; }
     
     public BreadOrders(int orderedAmount)
     {
       OrderAmount = orderedAmount;
+      Bill = 0;
+      FinalPrice = 0;
     }
 
     public int SumPrice()
     {
       int summedPrice = OrderAmount * 5;
+      Bill = summedPrice;
+      FinalPrice = summedPrice;
       return summedPrice;
+    }
+
+    public void CheckDiscount()
+    {
+      if (Bill % 3 == 0) 
+      {
+        FinalPrice = (Bill / 3) * 2;
+      }
+      else
+      {
+        FinalPrice = FinalPrice;
+      }
     }
   }
 }
