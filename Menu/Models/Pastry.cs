d0c7071a9a2f6b3b80b2c3947d3aae4aa64feb73
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-15 13:30:02 -0700

    implemented necessary methods

---
+++ Menu/Models/Pastry.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pastry.Models
+{
+  public class PastryOrders
+  {
+    public int OrderAmount { get; set; }
+    public int Bill { get; set; }
+    public int FinalPrice { get; set; }
+    
+    public PastryOrders(int orderedAmount)
+    {
+      OrderAmount = orderedAmount;
+      Bill = 0;
+      FinalPrice = 0;
+    }
+
+    public int SumPrice()
+    {
+      int summedPrice = OrderAmount * 2;
+      Bill = summedPrice;
+      FinalPrice = summedPrice;
+      return summedPrice;
+    }
+
+    public void CheckDiscount()
+    {
+      if (OrderAmount <= 5) 
+      {
+        FinalPrice = (Bill -1);
+      }
+      else if (OrderAmount > 5)
+      {
+        FinalPrice = (Bill - 2);
+      }
+      else
+      {
+        FinalPrice = FinalPrice;
+      }
+    }
+  }
+}
